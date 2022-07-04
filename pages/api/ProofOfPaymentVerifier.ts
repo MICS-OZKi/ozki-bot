6c8c798f79ee8dd8eeba6c393fff059b0c8e9670
0xA-ha <a-ha@berkeley.edu>
2022-07-04 02:35:58 -0700

    added ProofOfPaymentVerifier.ts

---
+++ pages/api/ProofOfPaymentVerifier.ts
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import  ProofVerifier from "ozki-lib";
+
+export default class ProofOfPaymentVerifier extends ProofVerifier<void> {
+    constructor(
+        zkpComponentPath: string,
+        zkpComponentName: string
+        ) {
+        console.log("#### >>ProofOfPaymentVerifier.ctor:");
+        super(zkpComponentPath, zkpComponentName);
+        console.log("#### <<ProofOfPaymentVerifier.ctor:");
+    }
+
+    /*
+    parseCustomOutput(customOutput: Array<string|null>): void {
+        console.log("#### >>ProofOfPaymentVerifier.parseCustomOutput:");
+        console.log("#### customOutput: len=%d, val=%s", customOutput.length, customOutput);
+        let status = false;
+
+        if (customOutput.length == 2) {
+            const [timeStamp, paymentStatus] = customOutput;
+            const number = Number.parseInt(paymentStatus);
+            status = (number == 1);
+        }
+
+        console.log("#### <<ProofOfPaymentVerifier.parseCustomOutput: status=%s", status);
+        return status;
+    }
+    */
+}
